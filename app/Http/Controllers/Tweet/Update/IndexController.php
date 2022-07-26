c5c90c43327bcf51202fb9b5ac2c51ce03a4aed9
Nobuhiko Funato <nfunato@acm.org>
2022-07-26 17:20:13 +0900

    update: complete chapter2 -- CRUD functions

---
+++ app/Http/Controllers/Tweet/Update/IndexController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Tweet\Update;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Tweet;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $tweetId = (int) $request->route('tweetId');
+        /*
+        $tweet = Tweet::where('id', $tweetId)->first();
+        if (is_null($tweet)) {
+            throw new NotFoundHttpException('存在しないつぶやきです');
+        }
+        */
+        $tweet = Tweet::where('id', $tweetId)->firstOrFail();
+        // dd($tweet);
+        return view('tweet.update')->with('tweet', $tweet);
+    }
+}
